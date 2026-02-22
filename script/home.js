c74cdf28fe18355d8791e151cd0eb4c76e64baac
unknown <asumona884@gmail.com>
2026-02-22 23:47:42 +0600

    resizing the html body

---
+++ script/home.js
@@ -0,0 +1,7 @@
+ document.getElementById('btn-interview').addEventListener('click', function(){
+// console.log('clicked');
+const interviewClicked = document.getElementById('interview-number');
+const clickedNumber = interviewClicked.innerText;
+console.log(clickedNumber);
+
+});
